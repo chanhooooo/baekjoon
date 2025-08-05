6712d78d4f08f37573a7f6efb327955b25afe056
chanhooooo <165656820+chanhooooo@users.noreply.github.com>
2025-08-05 09:57:34 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+# 스택
+# push(data
+# pop()
+# peek
+# is_empty()
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        if self.top == None:
+            self.top = Node(data)
+        else :
+            curr = Node(data)
+            curr.next = self.top
+            self.top = curr
+
+    def pop(self):
+        if self.top == None:
+            return None
+        else :
+            curr = self.top
+            self.top = self.top.next
+            return curr.data
+
+    def peek(self):
+        if self.top == None:
+            return None
+        else :
+            curr = self.top
+            return curr.data
+
+    def is_empty(self):
+        return self.top == None
+    
+#백준 12789 도키도키 간식드리미
+
+# 몇 명
+N = int(input())
+wait_list = list(map(int, input().split()))
+
+
+s = Stack()
+
+check = 1
+is_nice = True
+
+while len(wait_list) != 0:
+    if check == wait_list[0]:
+        del wait_list[0]
+        check += 1
+    else:
+        while s.peek() == check:
+            s.pop()
+            check += 1
+            
+        s.push(wait_list[0])
+        del wait_list[0]
+        
+while s.is_empty() == 0:    
+    if s.pop() == check:
+        check += 1
+    else:
+        is_nice = False
+        break
+
+print("Nice" if is_nice else "Sad")
