53c68d4929b38def56bd7a9a5c96fa5c856c0e06
chanhooooo <165656820+chanhooooo@users.noreply.github.com>
2025-09-05 19:58:11 +0900

    [Bronze II] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,12 +1,6 @@
-#3052 나머지
-
 arr1 = []
 for _ in range(10):
     N = int(input())
     arr1.append(N%42)
-uniqe = []
-for x in arr1:
-    if x not in uniqe:
-        uniqe.append(x)
 
-print(len(uniqe))
+print(len(set(arr1)))
