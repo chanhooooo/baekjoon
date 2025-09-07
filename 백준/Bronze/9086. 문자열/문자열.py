0aa90d804319253f0fbe76c8a1c2ca949c3cd68d
chanhooooo <165656820+chanhooooo@users.noreply.github.com>
2025-09-07 14:00:32 +0900

    [Bronze V] Title: 문자열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.py
+++ 백준/Bronze/9086. 문자열/문자열.py
@@ -1,10 +1,4 @@
-#9086 문자열
-
 N = int(input())
-str1 = []
-for i in range(N):
-    str1.append(input())
-
-for word in str1:
-    print(word[0], end="")
-    print(word[-1])
+for _ in range(N):
+    word = input()
+    print(word[0] + word[-1])
