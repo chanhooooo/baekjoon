5c67342a5f806277fa7c367a6ff4a1aff54ac77b
chanhooooo <165656820+chanhooooo@users.noreply.github.com>
2025-09-07 19:56:47 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,10 @@
+#3003 킹, 퀸, 룩, 비숍, 나이트, 폰
+#킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개
+
+k, q, l, b, n, p = map(int, input().split())
+print(1-k, end = " ")
+print(1-q, end = " ")
+print(2-l, end = " ")
+print(2-b, end = " ")
+print(2-n, end = " ")
+print(8-p, end = "")
